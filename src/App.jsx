7c57212422c77d17507eaf37e8d50ed0a14768ad
mehrsa <sirousmehr.mehrsa21@gmail.com>
2025-09-28 00:09:21 +0330

    use createEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import Header from "./components/Header";
-import { createSignal } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 
 const App = () => {
   console.log("App")
     const [counter, setCounter] = createSignal(0);
+    const dobleCounter = () => counter() * 2;
 
-    setInterval(() => {
-        setCounter(counter() + 1);
-    }, 1000);
+    // createEffect(() => {
+    //     console.log("effect", counter())
+    // })
+
+    // setInterval(() => {
+    //     setCounter(counter() + 1);
+    // }, 1000);
 
     return (
         <>
             <Header />
-            <p id="counter">{counter()}</p>
+            {/* <p id="counter">{counter()}</p> */}
+            <p id="counter">{dobleCounter()}</p>
             <div id="actions">
                 <button onclick={() => setCounter(counter() - 1)}>-</button>
                 <button onclick={() => setCounter((prevCounter) => prevCounter + 1)}>+</button>
